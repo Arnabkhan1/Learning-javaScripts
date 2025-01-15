b0cb9f54cbdaa352f9564e4850c36e6fe2a4e6f2
Arnab khan <145889647+Arnabkhan1@users.noreply.github.com>
2025-01-15 18:23:40 +0000

    Second push

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 13348;
+let accountEmail = "Khanarnab12345@gmail.com";
+var accountPassword = "999";
+accountCity = "Kolkata";
+let accountState;
+
+// accountId = 333;
+
+/*
+ var not use because block scope and functional scope
+*/
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all js code as newer version
+
+//alert(3 + 3); // using node js not use browser
+
+
+// Data types
+
+
+// Number 
+// String
+//boolean
+//null
+//undefined
+//bigint
+//symbol
+
+let number = 123;
+let name = "Arnab";
+let isAdmin = true;
+let bug = null;
+let un;
+
+console.table([number, name, isAdmin, bug, un]);
